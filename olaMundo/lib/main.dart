f4699c958f1965f51ab0e669c864a383f457947a
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-29 08:11:32 -0300

    #8 - Estados (StatefulWidget)

--- olaMundo/lib/main.dart
+++ olaMundo/lib/main.dart
@@ -16,9 +16,32 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red
       ),
-      home: Container(
-        child: Center(child: Text('Vitor')),
-      ),
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+
+}
+
+class HomePageState extends State<HomePage> {
+
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(child: GestureDetector(child: Text('Contador: $counter'),
+        onTap: (){
+          setState(() {
+            counter++;
+          });
+        },)),
+      );
   }
 }
