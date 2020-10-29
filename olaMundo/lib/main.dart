76eecdcfcf95ca5aeb7ca1d753b50a5ce72f3240
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-28 21:08:08 -0300

    #7 - O MaterialApp

--- olaMundo/lib/main.dart
+++ olaMundo/lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-      'Flutter',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(color: Colors.white, fontSize: 50.0),
-  ),
-    ),
-  ));
+  runApp(AppWidget(title: 'Vitor',));
+}
+
+class AppWidget extends StatelessWidget {
+
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red
+      ),
+      home: Container(
+        child: Center(child: Text('Vitor')),
+      ),
+    );
+  }
 }
