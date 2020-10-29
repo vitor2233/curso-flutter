216cdc3bc0e478c284fc19a5ec20faa8229f5804
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-29 08:18:45 -0300

    #9 - Boas Práticas e organização

--- olaMundo/lib/main.dart
+++ olaMundo/lib/main.dart
@@ -1,47 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 main() {
-  runApp(AppWidget(title: 'Vitor',));
+  runApp(AppWidget());
 }
 
-class AppWidget extends StatelessWidget {
-
-  final String title;
-
-  const AppWidget({Key key, this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-
-}
-
-class HomePageState extends State<HomePage> {
-
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Center(child: GestureDetector(child: Text('Contador: $counter'),
-        onTap: (){
-          setState(() {
-            counter++;
-          });
-        },)),
-      );
-  }
-}
