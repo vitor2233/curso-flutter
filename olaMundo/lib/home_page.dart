80429fc654841adc5c24c8b17e5c97a7dbf06e47
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-29 11:47:19 -0300

    #14 - Columns

--- olaMundo/lib/home_page.dart
+++ olaMundo/lib/home_page.dart
@@ -18,7 +18,17 @@ class HomePageState extends State<HomePage> {
         title: Center(child: Text('Vitor')),
         actions: [CustomSwitch()],
       ),
-      body: Center(child: CustomSwitch()),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Contador: $counter'),
+            CustomSwitch(),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
