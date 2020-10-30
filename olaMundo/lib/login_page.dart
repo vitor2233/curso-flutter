8665c95baebddb7827df141533183805d517135d
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-30 08:48:52 -0300

    #23 - Stack

--- olaMundo/lib/login_page.dart
+++ olaMundo/lib/login_page.dart
@@ -9,60 +9,75 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String senha = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset('assets/images/pp.png')),
-                Container(height: 50),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset('assets/images/pp.png')),
+              Container(height: 50),
+              TextField(
+                onChanged: (text) {
+                  email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
                 ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    senha = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
-                  ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                onChanged: (text) {
+                  senha = text;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  border: OutlineInputBorder(),
                 ),
-                SizedBox(height: 15),
-                RaisedButton(
-                  onPressed: () {
-                    if (email == 'vitor@email.com' && senha == '123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('inválido');
-                    }
-                  },
-                  child: Text('Entrar'),
-                )
-              ],
-            ),
+              ),
+              SizedBox(height: 15),
+              RaisedButton(
+                onPressed: () {
+                  if (email == 'vitor@email.com' && senha == '123') {
+                    Navigator.of(context).pushReplacementNamed('/home');
+                  } else {
+                    print('inválido');
+                  }
+                },
+                child: Text('Entrar'),
+              )
+            ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset(
+            'assets/images/back.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(color: Colors.black.withOpacity(0.4),),
+        _body(),
+      ],
+    ));
   }
 }
