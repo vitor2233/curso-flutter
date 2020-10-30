d33ad00f9f3ec58003f55cd48e0d031d1998c0e1
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-30 09:41:32 -0300

    #25 - Drawer

--- olaMundo/lib/home_page.dart
+++ olaMundo/lib/home_page.dart
@@ -14,6 +14,32 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: Image.asset('assets/images/pp.png'),
+              accountName: Text('Vítor Guilherme'),
+              accountEmail: Text('vitor@email.com')),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Início'),
+              subtitle: Text('Tela de início'),
+              onTap: (){
+                print('Home');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Logout'),
+              subtitle: Text('Finalizar sessão'),
+              onTap: (){
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Center(child: Text('Vitor')),
         actions: [CustomSwitch()],
