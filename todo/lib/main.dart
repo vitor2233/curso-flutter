97c3b56dcc5cc280f0c0922b764558c1a3f2fab2
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-30 11:16:04 -0300

    Aula 3: Material App

---
+++ todo/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
