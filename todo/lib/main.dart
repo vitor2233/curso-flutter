4036946214a8495899f73b05ffcb6698e6d61c68
Vitor Guilherme Moreira Pedro <vitor.guilherme@concert.com.br>
2020-10-30 13:19:22 -0300

    Aula 6: ListView

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/item.dart';
+
 void main() {
   runApp(App());
 }
@@ -18,7 +20,21 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    items.add(Item(title: 'Item 1', done: false));
+    items.add(Item(title: 'Item 2', done: false));
+    items.add(Item(title: 'Item 3', done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,8 +44,11 @@ class HomePage extends StatelessWidget {
           Icon(Icons.bookmark_border),
         ],
       ),
-      body: Container(
-        child: Center(child: Text('Olá mundo')),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          return Text(widget.items[index].title);
+        },
       ),
     );
   }
